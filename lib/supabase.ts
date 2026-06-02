9a9fbc2dc0e6e95b59bf7c2a39ace77cde740684
Agonie-dev <1031262355@qq.com>
2026-06-02 22:13:13 +0800

    chore: update supabase config to new project

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kesuyxfvfkcpyrgfoofm.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtlc3V5eGZ2ZmtjcHlyZ2Zvb2ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzOTU3MDIsImV4cCI6MjA5NTk3MTcwMn0.rnxdi_MIc1eHhDTo0Vn2coryIKd9Ui9hf8xDH6W_nq0'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
